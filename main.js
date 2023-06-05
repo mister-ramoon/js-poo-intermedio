59c9fbe4a3015ed71a6ed68aa1d9b043860dc12f
ramonruiz <jruiz@lightia.mx>
2023-06-05 10:21:43 -0600

    Object.defineProperty

--- main.js
+++ main.js
@@ -10,13 +10,39 @@ const juan = {
 };
 
 //juan.addCourse("Curso 2");
-console.log(Object.keys(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
-console.log(Object.getOwnPropertyNames(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
-console.log(Object.entries(juan)); // [["nombre", "Juan"], ["age", 18], ["approvedCourses", ["Curso 1"]], ["addCourse", f]]
-console.log(Object.getOwnPropertyDescriptors(juan)); // {nombre: {…}, age: {…}, approvedCourses: {…}, addCourse: {…}}
-Object.defineProperty(juan, "pruebaNASA", {
-  value: "Extraterrestres",
-  enumerable: true,
+//console.log(Object.keys(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
+//console.log(Object.getOwnPropertyNames(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
+//console.log(Object.entries(juan)); // [["nombre", "Juan"], ["age", 18], ["approvedCourses", ["Curso 1"]], ["addCourse", f]]
+
+Object.defineProperty(juan, "navigator", {
+  value: "Chrome",
+  enumerable: false,
   writable: true,
   configurable: true,
 });
+
+Object.defineProperty(juan, "pruebaNASA", {
+  value: "Extraterrestres",
+  enumerable: false,
+  writable: false,
+  configurable: false,
+});
+
+Object.defineProperty(juan, "editor", {
+  value: "VS Code",
+  enumerable: true,
+  writable: false,
+  configurable: true,
+});
+
+Object.defineProperty(juan, "terminal", {
+  value: "wsl",
+  enumerable: true,
+  writable: true,
+  configurable: false,
+});
+
+// console.log(Object.freeze(juan));
+// console.log(Object.seal(juan));
+
+console.log(Object.getOwnPropertyDescriptors(juan)); // {nombre: {…}, age: {…}, approvedCourses: {…}, addCourse: {…}}
