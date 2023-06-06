674d9b7bc2de2d8521278e09c594db511a394628
ramonruiz <jruiz@lightia.mx>
2023-06-06 09:42:43 -0600

    Shallow copy en JavaScript

--- main.js
+++ main.js
@@ -1,48 +1,17 @@
-const juan = {
-  name: "Juan",
-  age: 18,
-  approvedCourses: ["Curso 1"],
-  addCourse(newCourse) {
-    console.log("this", this);
-    console.log("this.approvedCourses", this.approvedCourses);
-    this.approvedCourses.push(newCourse);
+const obj1 = {
+  a: "a",
+  b: "b",
+  c: {
+    d: "d",
+    e: "e",
   },
 };
 
-//juan.addCourse("Curso 2");
-//console.log(Object.keys(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
-//console.log(Object.getOwnPropertyNames(juan)); // ["nombre", "age", "approvedCourses", "addCourse"]
-//console.log(Object.entries(juan)); // [["nombre", "Juan"], ["age", 18], ["approvedCourses", ["Curso 1"]], ["addCourse", f]]
+const obj2 = {};
 
-Object.defineProperty(juan, "navigator", {
-  value: "Chrome",
-  enumerable: false,
-  writable: true,
-  configurable: true,
-});
+for (prop in obj1) {
+  obj2[prop] = obj1[prop];
+}
 
-Object.defineProperty(juan, "pruebaNASA", {
-  value: "Extraterrestres",
-  enumerable: false,
-  writable: false,
-  configurable: false,
-});
-
-Object.defineProperty(juan, "editor", {
-  value: "VS Code",
-  enumerable: true,
-  writable: false,
-  configurable: true,
-});
-
-Object.defineProperty(juan, "terminal", {
-  value: "wsl",
-  enumerable: true,
-  writable: true,
-  configurable: false,
-});
-
-// console.log(Object.freeze(juan));
-// console.log(Object.seal(juan));
-
-console.log(Object.getOwnPropertyDescriptors(juan)); // {nombre: {…}, age: {…}, approvedCourses: {…}, addCourse: {…}}
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
